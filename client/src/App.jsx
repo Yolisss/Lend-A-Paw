8deb9d1443ddf7b306b7c9053ca0c1975884739c
Yolis <yolisisabel28@gmail.com>
2023-05-04 12:21:20 -0700

    added form to app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavBar from "./components/Navbar";
 import ListAnimals from "./components/ListAnimals";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <MyNavBar />
       <ListAnimals />
+      <Form />
     </div>
   );
 }
