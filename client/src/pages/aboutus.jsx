e1502e4f89ff1dfbfee1ba42092e9e6de2a1325c
Yolis <yolisisabel28@gmail.com>
2023-05-04 22:43:07 -0700

    added images, containers, and lorum ipsum

--- client/src/pages/aboutus.jsx
+++ client/src/pages/aboutus.jsx
@@ -1,9 +1,64 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 
 export default function Aboutus() {
   return (
     <div>
-      <h1>About Us</h1>
+      <Container>
+        <img
+          src="https://d.newsweek.com/en/full/1809693/cat-dog.webp?w=1600&h=900&q=88&f=772f894f001bafc6c5094cc33d71bc19"
+          width="550px"
+          background-size:cover
+        />
+      </Container>
+      <Container>
+        <h3>Where It All Began</h3>
+        <img
+          src="https://www.catster.com/wp-content/uploads/2018/01/Orange-tabby-cat-sleeping-with-eyes-closed.jpg.optimal.jpg"
+          width="550px"
+        />
+        <p>
+          Cat ipsum dolor sit amet, malkin for siamese for abyssinian . Tiger
+          havana brown so kitty lynx. Bengal tomcat sphynx so abyssinian and
+          bombay but egyptian mau. Panther cornish rex and bombay or malkin, so
+          leopard manx and persian. Jaguar savannah yet russian blue malkin or
+          siberian and devonshire rex, and munchkin. Lynx leopard but ocicat and
+          ragdoll or ragdoll but grimalkin. Kitty puma but tomcat. Kitty
+          grimalkin for cornish rex. Jaguar singapura but maine coon and malkin
+          so ocelot yet bobcat. Tabby mouser for lynx. Tom ocicat mouser turkish
+          angora siamese. Turkish angora balinese lion. Jaguar singapura but
+          scottish fold and norwegian forest, bengal yet savannah yet norwegian
+          forest. Bengal. Puma tom yet sphynx grimalkin and british shorthair.
+          Himalayan norwegian forest lynx kitten kitty havana brown. Lion kitty
+          american shorthair american shorthair. Himalayan burmese, sphynx or
+          american shorthair mouser. Tom ragdoll or cheetah and malkin himalayan
+          malkin.
+        </p>
+      </Container>
+      <Container>
+        <img
+          src="https://media.npr.org/assets/img/2021/08/06/dog-4415649-18eab39206426b985f7a5f69e3146a2cd1a56c0d.jpg"
+          width="550px"
+        />
+      </Container>
+      <Container>
+        <h3>Our Mission</h3>
+        <p>
+          Walk on keyboard bite plants. Sleep nap. Lick master's hand at first
+          then bite because im moody. Love blinks and purr purr purr purr yawn
+          sit on human they not getting up ever and murr i hate humans they are
+          so annoying, rub against owner because nose is wet or human is behind
+          a closed door, emergency! abandoned! meeooowwww!!!, for car rides are
+          evil for litter box is life. Make it to the carpet before i vomit
+          mmmmmm do not try to mix old food with new one to fool me!. Why use
+          post when this sofa is here love to play with owner's hair tie but see
+          owner, run in terror. Stick butt in face ð•„ð”¼ð•†ð•Ž for i show my
+          fluffy belly but it's a trap! if you pet it i will tear up your hand
+          yet i like cats because they are fat and fluffy. Chew the plant purr
+          when give birth so cough furball into food bowl then scratch owner for
+          a new one, or time to go zooooom rub my belly hiss.{" "}
+        </p>
+      </Container>
     </div>
   );
 }
