f8c79085ab4820a9f73fa8c9438105882a92c262
Yolis <yolisisabel28@gmail.com>
2023-05-04 23:14:38 -0700

    imported list animals and form

--- client/src/pages/adoptablepets.jsx
+++ client/src/pages/adoptablepets.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import ListAnimals from "../components/ListAnimals";
+import Form from "../components/Form";
 
 export default function Adoptablepets() {
   return (
     <div>
       <h1>Adoptable Pets</h1>
+      <ListAnimals />
+      <Form />
     </div>
   );
 }
