4c49225c04116059cca66b11726b40c2de46a26f
Yolis <yolisisabel28@gmail.com>
2023-05-16 20:25:57 -0700

    centered image and paragraph

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,31 +2,44 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <div>
+    <div className="container">
+      <div className="homeimage">
         <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" />
       </div>
-      <p>
+      <div className="homeparagraph">
         "Saving just one pet won't save the world, but surely it will change the
-        wold for that one pet."
-      </p>
-      <div>
-        <img
-          className="homeimage"
-          src="https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg"
-          width="550px"
-        />
-        <p className="homeparagraph">
-          Ex doloremque, nihil consequat or nostrum consequuntur. Eius aliquip
-          nulla commodi rem sed. Nisi fugit quam suscipit so ratione, and
-          voluptas. Modi quasi. Eius non. Unde sunt or labore for quae or
-          commodi dolores, ullamco. Illum do so irure yet quasi numquam yet
-          cillum or aspernatur. Aspernatur adipisicing. Minima magna but
-          proident. Labore dicta ea for totam so commodi, aperiam for qui. Vitae
-          suscipit. Nisi ad anim for ipsam quia for vel, illum. Tempor iste
-          dolorem. Dolor error. Incididunt veniam, or inventore, yet cillum.
-        </p>
+        world for that one pet."
       </div>
     </div>
   );
 }
+
+// <div>
+//   <div className="container">
+//     <div class="firstcathomeimage">
+//       <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" />
+//     </div>
+//     <div className="homeparagraph">
+//       "Saving just one pet won't save the world, but surely it will change the
+//       wold for that one pet."
+//     </div>
+//   </div>
+// </div>;
+//  <div className="container2">
+//    <div className="firstcathomeimage2">
+//      <img
+//        src="https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg"
+//        width="550px"
+//      />
+//    </div>
+//    <div className="homeparagraph2">
+//      Ex doloremque, nihil consequat or nostrum consequuntur. Eius aliquip nulla
+//      commodi rem sed. Nisi fugit quam suscipit so ratione, and voluptas. Modi
+//      quasi. Eius non. Unde sunt or labore for quae or commodi dolores, ullamco.
+//      Illum do so irure yet quasi numquam yet cillum or aspernatur. Aspernatur
+//      adipisicing. Minima magna but proident. Labore dicta ea for totam so
+//      commodi, aperiam for qui. Vitae suscipit. Nisi ad anim for ipsam quia for
+//      vel, illum. Tempor iste dolorem. Dolor error. Incididunt veniam, or
+//      inventore, yet cillum.
+//    </div>
+//  </div>;
