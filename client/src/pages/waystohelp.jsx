a062813c1fd39bdbd920749468bca2a38e74f7a8
Yolis <yolisisabel28@gmail.com>
2023-05-19 12:14:25 -0700

    modified card components in waystohelp by changing from row to col and added each component in its own div

--- client/src/pages/waystohelp.jsx
+++ client/src/pages/waystohelp.jsx
@@ -12,8 +12,8 @@ export default function Waystohelp() {
     <div>
       <div className="waystohelpimage2">
         <img
+          className="img-fluid"
           src="https://post.healthline.com/wp-content/uploads/2020/07/petting_pet_cat-1200x628-facebook-1200x628.jpg"
-          width="550px"
         />
       </div>
 
@@ -23,7 +23,7 @@ export default function Waystohelp() {
           fur friend, feed it and keept it around."
         </p>
       </div>
-      <div className="waystohelpcontent2">
+      <div>
         <div
           className="waystohelpimages1"
           style={{
@@ -31,35 +31,38 @@ export default function Waystohelp() {
               "url(https://dogsbestlife.com/wp-content/uploads/2021/02/cats-vs.-dogs-group-scaled.jpeg)",
           }}
         ></div>
-        <p>
-          To pet a cat, rub its belly, endure blood and agony, quietly weep,
-          keep rubbing belly chew foot, for funny little cat chirrup noise
-          shaking upright tail when standing next to you. Purr while eating poop
-          in litter box, scratch the walls so snuggles up to shoulders or knees
-          and purrs you to sleep pushed the mug off the table so hide when
-          guests come over kitty kitty pussy cat doll, but intrigued by the
-          shower. Stare out cat door then go back inside. Cry louder at
-          reflection lick butt and make a weird face but stare out cat door then
-          go back inside but eat plants, meow, and throw up because i ate
-          plants. Scratch the box check cat door for ambush 10 times before
-          coming in yet sun bathe give attitude. Eat grass, throw it back up
-          annoy kitten brother with poking, iâ€™m so hungry iâ€™m so hungry but
-          ew not for that chase dog then run away stare at wall turn and meow
-          stare at wall some more meow again continue staring , but tickle my
-          belly at your own peril i will pester for food when you're in the
-          kitchen even if it's salad but human is in bath tub, emergency!
-          drowning! meooowww!.
-        </p>
+        <div className="waystohelpparag">
+          <p>
+            To pet a cat, rub its belly, endure blood and agony, quietly weep,
+            keep rubbing belly chew foot, for funny little cat chirrup noise
+            shaking upright tail when standing next to you. Purr while eating
+            poop in litter box, scratch the walls so snuggles up to shoulders or
+            knees and purrs you to sleep pushed the mug off the table so hide
+            when guests come over kitty kitty pussy cat doll, but intrigued by
+            the shower. Stare out cat door then go back inside. Cry louder at
+            reflection lick butt and make a weird face but stare out cat door
+            then go back inside but eat plants, meow, and throw up because i ate
+            plants. Scratch the box check cat door for ambush 10 times before
+            coming in yet sun bathe give attitude. Eat grass, throw it back up
+            annoy kitten brother with poking, iâ€™m so hungry iâ€™m so hungry
+            but ew not for that chase dog then run away stare at wall turn and
+            meow stare at wall some more meow again continue staring , but
+            tickle my belly at your own peril i will pester for food when you're
+            in the kitchen even if it's salad but human is in bath tub,
+            emergency! drowning! meooowww!.
+          </p>
+        </div>
       </div>
-      <div class="row pt-5">
-        <div class="row-md-4">
-          <Card>
-            <CardContent className="p-4 d-flex flex-column justify-content-center">
+      <div className="container-fluid pt-5">
+        <Card>
+          <CardContent className="p-4 row align-items-center">
+            <div className="col-md-4">
               <img
+                className="img-fluid"
                 src="https://images.squarespace-cdn.com/content/v1/5cd493f1185add0001e4670d/1558988155986-PY602J3JHUN77WUMVRXS/image-asset.jpeg?format=2500w"
-                width="50%"
               />
-              <br></br>
+            </div>
+            <div className="col-md-8">
               <CardDescription>
                 Deciding to adopt a rescue pet or shelter dog is an important
                 decision. It can be tough to take into account everything you’ll
@@ -147,17 +150,13 @@ export default function Waystohelp() {
                 /> */}
                 </Modal.Actions>
               </Modal>
-            </CardContent>
-          </Card>
-        </div>
-        <div class="row-md-4">
-          <Card>
-            <CardContent className="p-4 d-flex flex-column justify-content-center">
-              <img
-                src="https://img1.wsimg.com/isteam/ip/9a5a6325-6df0-41ed-a195-fd5ab6ea633e/AF4BFC1D-1716-4230-9366-2F84269AF510.jpeg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=h:1000,cg:true"
-                width="50%"
-              />
-              <br></br>
+            </div>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardContent className="p-4 row align-items-center">
+            <div className="col-md-8">
               <CardDescription>
                 We’re sure you’ve heard that fostering animals in need is
                 important, and even lifesaving, but do you know why? Our top
@@ -257,90 +256,95 @@ export default function Waystohelp() {
                 /> */}
                 </Modal.Actions>
               </Modal>
-            </CardContent>
-          </Card>
-        </div>
-        <div class="row-md-4">
-          <Card>
-            <CardContent className="p-4 d-flex flex-column justify-content-center">
+            </div>
+            <div className="col-md-4">
               <img
-                src="https://images.contentstack.io/v3/assets/blt6f84e20c72a89efa/blt0dea19c9ca1bfc7e/6261d5d5479b5e559dd84bc2/img-things-donate-animal-shelters-header.jpg"
-                width="50%"
+                className="img-fluid"
+                src="https://img1.wsimg.com/isteam/ip/9a5a6325-6df0-41ed-a195-fd5ab6ea633e/AF4BFC1D-1716-4230-9366-2F84269AF510.jpeg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=h:1000,cg:true"
               />
-              <br></br>
-              <CardDescription>
-                The people that work at these shelters do a lot for the dogs
-                under their care. That is one reason why you should donate to
-                dog shelters. You are helping your neighbors earn money, help
-                the economy as well as help the many unwanted dogs who did not
-                ask to be there.
-              </CardDescription>
-              <Modal
-                onClose={() => setOpen2(false)}
-                onOpen={() => setOpen2(true)}
-                open={open2}
-                trigger={
-                  <div className="text-center">
-                    <Button className="mt-4">Learn More About Donating</Button>
-                  </div>
-                }
-              >
-                <Modal.Header>
-                  Why You Should Donate to Animal Shelters
-                </Modal.Header>
-                <Modal.Content image>
-                  <Image
-                    size="large"
-                    src="https://mymodernmet.com/wp/wp-content/uploads/2022/02/cute-cat-and-dog-friends-1.jpg"
-                    wrapped
-                  />
-                  <Modal.Description>
-                    <ul>
-                      <li>
-                        <h4>1. Houses More Dogs</h4>
-                        Your donations make it possible for these dog shelters
-                        to help more dogs find safety, shelter, and a new home.
-                        The longer a dog can stay in the shelter the better
-                        their chances of finding a new owner they can be a
-                        companion to and protect.
-                      </li>
-                      <li>
-                        <h4>2. Cuts Down on Euthanasion</h4>
-                        Those dog shelters that get donations can avoid the
-                        tragic event of putting unwanted dogs down. This is very
-                        emotional for the shelter workers, something they would
-                        rather avoid.Plus, with the extra funds, they can keep
-                        those dogs longer until a good home is found for them.
-                        Your donation can save dogs as well as spare humans this
-                        emotional time.
-                      </li>
-                      <li>
-                        <h4>3. If You Can't Adopt, You Can Still Help</h4>
-                        That is the bottom line here. Not everyone can adopt a
-                        dog. Their living situations just do not allow it.
-                        However, you can still help by making donations to dog
-                        shelters allowing someone else who can adopt to have the
-                        time to find the right pet for them.
-                      </li>
-                    </ul>
-                  </Modal.Description>
-                </Modal.Content>
-                <Modal.Actions>
-                  <Button color="black" onClick={() => setOpen2(false)}>
-                    Close
-                  </Button>
-                  {/* <Button
+            </div>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardContent className="p-4 d-flex flex-column justify-content-center">
+            <img
+              src="https://images.contentstack.io/v3/assets/blt6f84e20c72a89efa/blt0dea19c9ca1bfc7e/6261d5d5479b5e559dd84bc2/img-things-donate-animal-shelters-header.jpg"
+              width="50%"
+            />
+            <br></br>
+            <CardDescription style={{ textAlign: "center", padding: "40px" }}>
+              The people that work at these shelters do a lot for the dogs under
+              their care. That is one reason why you should donate to dog
+              shelters. You are helping your neighbors earn money, help the
+              economy as well as help the many unwanted dogs who did not ask to
+              be there.
+            </CardDescription>
+            <Modal
+              onClose={() => setOpen2(false)}
+              onOpen={() => setOpen2(true)}
+              open={open2}
+              trigger={
+                <div className="text-center">
+                  <Button className="mt-4">Learn More About Donating</Button>
+                </div>
+              }
+            >
+              <Modal.Header>
+                Why You Should Donate to Animal Shelters
+              </Modal.Header>
+              <Modal.Content image>
+                <Image
+                  size="large"
+                  src="https://mymodernmet.com/wp/wp-content/uploads/2022/02/cute-cat-and-dog-friends-1.jpg"
+                  wrapped
+                />
+                <Modal.Description>
+                  <ul>
+                    <li>
+                      <h4>1. Houses More Dogs</h4>
+                      Your donations make it possible for these dog shelters to
+                      help more dogs find safety, shelter, and a new home. The
+                      longer a dog can stay in the shelter the better their
+                      chances of finding a new owner they can be a companion to
+                      and protect.
+                    </li>
+                    <li>
+                      <h4>2. Cuts Down on Euthanasion</h4>
+                      Those dog shelters that get donations can avoid the tragic
+                      event of putting unwanted dogs down. This is very
+                      emotional for the shelter workers, something they would
+                      rather avoid.Plus, with the extra funds, they can keep
+                      those dogs longer until a good home is found for them.
+                      Your donation can save dogs as well as spare humans this
+                      emotional time.
+                    </li>
+                    <li>
+                      <h4>3. If You Can't Adopt, You Can Still Help</h4>
+                      That is the bottom line here. Not everyone can adopt a
+                      dog. Their living situations just do not allow it.
+                      However, you can still help by making donations to dog
+                      shelters allowing someone else who can adopt to have the
+                      time to find the right pet for them.
+                    </li>
+                  </ul>
+                </Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button color="black" onClick={() => setOpen2(false)}>
+                  Close
+                </Button>
+                {/* <Button
                   content="Yep, that's me"
                   labelPosition="right"
                   icon="checkmark"
                   onClick={() => setOpen(false)}
                   positive
                 /> */}
-                </Modal.Actions>
-              </Modal>
-            </CardContent>
-          </Card>
-        </div>
+              </Modal.Actions>
+            </Modal>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
