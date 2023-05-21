916c9c69bc5a3fc2dcea90756057e08abd43eff6
Yolis <yolisisabel28@gmail.com>
2023-05-20 22:40:49 -0700

    played with padding for three images in adoption page however will need to find out how to center it

--- client/src/pages/adoptionpage.jsx
+++ client/src/pages/adoptionpage.jsx
@@ -7,25 +7,25 @@ export default function Adoptionpage() {
   const { animalid } = useParams();
   return (
     <div>
-      <div className="thankyouheader">
-        <h3>Thank You For Considering Adoption!</h3>
-      </div>
-      <div className="thankyouparagraph">
-        Annoy owner until he gives you food say meow repeatedly until belly
-        rubs, feels good missing until dinner time, yet meow meow pee in shoe
-        sit by the fire, play time, so climb leg, for i want to go outside let
-        me go outside nevermind inside is better. Thug cat sit on human scratch
-        at door to be let outside, get let out then scratch at door immmediately
-        after to be let back in yet cat playing a fiddle in hey diddle diddle?
-        hiss at vacuum cleaner what a cat-ass-trophy! for fall asleep
-        upside-down. Cough furball into food bowl then scratch owner for a new
-        one you have cat to be kitten me right meow where is it? i saw that bird
-        i need to bring it home to mommy squirrel! or scream for no reason at 4
-        am. Enslave the hooman stare out cat door then go back inside so somehow
-        manage to catch a bird but have no idea what to do next, so play with it
-        until it dies of shock roll on the floor purring your whiskers off but
-        what the heck just happened, something feels fishy, cat cat moo moo lick
-        ears lick paws.{" "}
+      <div className="thankyoucomponent">
+        <h3 className="thankyouheader">Thank You For Considering Adoption!</h3>
+        <p>
+          Annoy owner until he gives you food say meow repeatedly until belly
+          rubs, feels good missing until dinner time, yet meow meow pee in shoe
+          sit by the fire, play time, so climb leg, for i want to go outside let
+          me go outside nevermind inside is better. Thug cat sit on human
+          scratch at door to be let outside, get let out then scratch at door
+          immmediately after to be let back in yet cat playing a fiddle in hey
+          diddle diddle? hiss at vacuum cleaner what a cat-ass-trophy! for fall
+          asleep upside-down. Cough furball into food bowl then scratch owner
+          for a new one you have cat to be kitten me right meow where is it? i
+          saw that bird i need to bring it home to mommy squirrel! or scream for
+          no reason at 4 am. Enslave the hooman stare out cat door then go back
+          inside so somehow manage to catch a bird but have no idea what to do
+          next, so play with it until it dies of shock roll on the floor purring
+          your whiskers off but what the heck just happened, something feels
+          fishy, cat cat moo moo lick ears lick paws.{" "}
+        </p>
       </div>
       <div className="thankyoucolumn">
         <div className="thankyourow">
