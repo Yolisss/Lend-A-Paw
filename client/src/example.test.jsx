9f5fbf32e354e3bf84f961bb75f873830b70d66b
Yolis <yolisisabel28@gmail.com>
2023-05-23 15:45:31 -0700

    added third testing for contactinfo component

--- client/src/example.test.jsx
+++ client/src/example.test.jsx
@@ -2,6 +2,8 @@ import MyNavBar from "./components/Navbar";
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import Catordog from "./components/catordog";
+import Contactinfo from "./components/contactinfo";
 
 //Test about Navbar
 test("Navbar renders correctly", () => {
@@ -33,4 +35,25 @@ test("Check that the button login is in the document", () => {
   );
   const button = screen.getByText("Log In");
   expect(button).toBeDefined();
+});
+
+test("catordog renders correctly", () => {
+  //render function will take react and display it in a
+  //virtual browser
+  const { getByTestId } = render(
+    //to make context exist, you wrap it around with
+    //Browser router
+    //render provides functions that can be later used inthe
+    //virtual browser to access or interact w elem i.e { getByTestId}
+    //{ getByTestId } => similar to the element selector (box w arrow from inspector tools)
+    <BrowserRouter>
+      <Contactinfo />
+    </BrowserRouter>
+  );
+  //selecting that elem and storing a reference to it
+  //now you can perform any action to it
+  //you can check if it has a className
+  const contactInfoListElement = getByTestId("contactinfolist");
+  //"we expect it to exist after the run"
+  expect(contactInfoListElement).toBeDefined();
 });
