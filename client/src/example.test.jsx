36e7dfd110cbd97717fa5390634018300b91def9
Yolis <yolisisabel28@gmail.com>
2023-05-23 19:36:15 -0700

    added a test for form component however failed

--- client/src/example.test.jsx
+++ client/src/example.test.jsx
@@ -4,6 +4,7 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Catordog from "./components/catordog";
 import Contactinfo from "./components/contactinfo";
+import { Form } from "react-bootstrap";
 
 //Test about Navbar
 test("Navbar renders correctly", () => {
@@ -56,4 +57,34 @@ test("catordog renders correctly", () => {
   const contactInfoListElement = getByTestId("contactinfolist");
   //"we expect it to exist after the run"
   expect(contactInfoListElement).toBeDefined();
+});
+
+test("Form renders correctly", () => {
+  //render function will take react and display it in a
+  //virtual browser
+  const { getByTestId } = render(
+    //to make context exist, you wrap it around with
+    //Browser router
+    //render provides functions that can be later used inthe
+    //virtual browser to access or interact w elem i.e { getByTestId}
+    //{ getByTestId } => similar to the element selector (box w arrow from inspector tools)
+    <BrowserRouter>
+      <Contactinfo />
+    </BrowserRouter>
+  );
+  //selecting that elem and storing a reference to it
+  //now you can perform any action to it
+  //you can check if it has a className
+  const contactInfoListElement = getByTestId("formtest");
+  //"we expect it to exist after the run"
+  expect(contactInfoListElement).toBeDefined();
+});
+test("Check that the Full Name is in the document", () => {
+  render(
+    <BrowserRouter>
+      <Form />
+    </BrowserRouter>
+  );
+  const button = screen.getByText("Email");
+  expect(button).toBeDefined();
 });
