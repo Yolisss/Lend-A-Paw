e2746ed84de22fbc14c2aaa078164eaf53e65f2b
Yolis <yolisisabel28@gmail.com>
2023-05-25 11:22:51 -0700

    comment out form testing from main test file

--- client/src/example.test.jsx
+++ client/src/example.test.jsx
@@ -59,32 +59,32 @@ test("catordog renders correctly", () => {
   expect(contactInfoListElement).toBeDefined();
 });
 
-test("Form renders correctly", () => {
-  //render function will take react and display it in a
-  //virtual browser
-  const { getByTestId } = render(
-    //to make context exist, you wrap it around with
-    //Browser router
-    //render provides functions that can be later used inthe
-    //virtual browser to access or interact w elem i.e { getByTestId}
-    //{ getByTestId } => similar to the element selector (box w arrow from inspector tools)
-    <BrowserRouter>
-      <Contactinfo />
-    </BrowserRouter>
-  );
-  //selecting that elem and storing a reference to it
-  //now you can perform any action to it
-  //you can check if it has a className
-  const contactInfoListElement = getByTestId("formtest");
-  //"we expect it to exist after the run"
-  expect(contactInfoListElement).toBeDefined();
-});
-test("Check that the Full Name is in the document", () => {
-  render(
-    <BrowserRouter>
-      <Form />
-    </BrowserRouter>
-  );
-  const button = screen.getByText("Email");
-  expect(button).toBeDefined();
-});
+// test("Form renders correctly", () => {
+//   //render function will take react and display it in a
+//   //virtual browser
+//   const { getByTestId } = render(
+//     //to make context exist, you wrap it around with
+//     //Browser router
+//     //render provides functions that can be later used inthe
+//     //virtual browser to access or interact w elem i.e { getByTestId}
+//     //{ getByTestId } => similar to the element selector (box w arrow from inspector tools)
+//     <BrowserRouter>
+//       <Contactinfo />
+//     </BrowserRouter>
+//   );
+//   //selecting that elem and storing a reference to it
+//   //now you can perform any action to it
+//   //you can check if it has a className
+//   const contactInfoListElement = getByTestId("formtest");
+//   //"we expect it to exist after the run"
+//   expect(contactInfoListElement).toBeDefined();
+// });
+// test("Check that the Full Name is in the document", () => {
+//   render(
+//     <BrowserRouter>
+//       <Form />
+//     </BrowserRouter>
+//   );
+//   const button = screen.getByText("Email");
+//   expect(button).toBeDefined();
+// });
